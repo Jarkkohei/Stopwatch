50e7e805301da30c3dd9339725843f11dc58d458
Jarkko <jarkkohei@luukku.com>
2018-06-30 13:09:05 +0300

    Add the 'Timer'-component

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import moment from 'moment';
 
-export default class App extends React.Component {
+const DATA = {
+  timer: 1234567,
+  laps: [ 12345, 23456, 34567, 98765 ],
+};
+
+function Timer({ interval }) {
+  const duration = moment.duration(interval);
+  const centiseconds = Math.floor(duration.milliseconds() / 10);
+
+  return (
+    <Text style={ styles.timer }>
+      {duration.minutes()}:{duration.seconds()},{centiseconds}
+    </Text>
+  );
+}
+
+class App extends Component {
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Terve!</Text>
+      <View style={ styles.container }>
+        <Timer interval={ DATA.timer } />
       </View>
     );
   }
@@ -13,9 +31,17 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 25,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0fa',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 100
   },
+  timer: {
+    color: '#fff',
+    fontSize: 76,
+    fontWeight: '200'
+  }
 });
+
+export default App;
