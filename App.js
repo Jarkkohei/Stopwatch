999fe297899bb6e862307ca1f002f816b833e83e
Jarkko <jarkkohei@luukku.com>
2018-06-30 13:48:17 +0300

    Add 'ButtonsRow'- and 'RoundButton'-components as 'Reset'- and 'Start'-buttons

--- App.js
+++ App.js
@@ -7,7 +7,9 @@ const DATA = {
   laps: [ 12345, 23456, 34567, 98765 ],
 };
 
-function Timer({ interval }) {
+
+
+Timer = ({ interval }) => {
   const duration = moment.duration(interval);
   const centiseconds = Math.floor(duration.milliseconds() / 10);
 
@@ -16,7 +18,28 @@ function Timer({ interval }) {
       {duration.minutes()}:{duration.seconds()},{centiseconds}
     </Text>
   );
-}
+};
+
+
+RoundButton = ({ title, color, background }) => {
+  return(
+    <View style={[ styles.button, { backgroundColor: background }]}>
+      <View style={ styles.buttonBorder }>
+        <Text style={[ styles.buttonTitle, { color }]}>{ title }</Text>
+      </View>
+    </View>
+  );
+};
+
+
+ButtonsRow = ({ children }) => {
+  return(
+    <View style={ styles.buttonsRow }>
+      { children }
+    </View>
+  );
+};
+
 
 class App extends Component {
 
@@ -24,6 +47,10 @@ class App extends Component {
     return (
       <View style={ styles.container }>
         <Timer interval={ DATA.timer } />
+        <ButtonsRow>
+          <RoundButton title='Reset' color='#fff' background='#666' />
+          <RoundButton title='Start' color='#fff' background='#090' />
+        </ButtonsRow>
       </View>
     );
   }
@@ -35,12 +62,39 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#0fa',
     alignItems: 'center',
-    paddingTop: 100
+    paddingTop: 100,
+    paddingHorizontal: 20,
   },
   timer: {
     color: '#fff',
     fontSize: 76,
     fontWeight: '200'
+  },
+  button: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonTitle: {
+    fontSize: 18,
+
+  },
+  buttonBorder: {
+    width: 76,
+    height: 76,
+    borderRadius: 38,
+    borderWidth: 2,
+    borderColor: '#0fa',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonsRow: {
+    flexDirection: 'row',
+    alignSelf: 'stretch',
+    justifyContent: 'space-between',
+    marginTop: 80
   }
 });
 
